8a2d02dad08cddaa320ff52ec8cb90f88254b888
Anton Shadrin <a.shadrin@yadro.com>
2025-05-14 03:48:11 +0300

    Add job and cronjob related methods

--- servers/backup/jobs.go
+++ servers/backup/jobs.go
@@ -7,17 +7,26 @@ import (
 	batchv1 "k8s.io/api/batch/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
-func (s *BackupServer) createJob(ctx context.Context, jobSpec *batchv1.Job) (string, string, error) {
-	exCj, err := s.kubeClient.BatchV1().Jobs(jobSpec.Namespace).Get(ctx, jobSpec.Name, metav1.GetOptions{})
+type JobsCreationError = error
+
+var ErrAlreadyExists JobsCreationError = fmt.Errorf("already exists")
+
+type JobsCreator struct {
+	kubeClient *kubernetes.Clientset
+}
+
+func (jc JobsCreator) CreateJob(ctx context.Context, jobSpec *batchv1.Job) (string, string, error) {
+	exCj, err := jc.kubeClient.BatchV1().Jobs(jobSpec.Namespace).Get(ctx, jobSpec.Name, metav1.GetOptions{})
 	if apierrors.IsAlreadyExists(err) {
 		return exCj.Name, exCj.Namespace, ErrAlreadyExists
 	}
 	if err != nil && !apierrors.IsNotFound(err) {
 		return "", "", fmt.Errorf("Failed to check cj %s existence: %w", jobSpec.Name, err)
 	}
-	generatedJob, err := s.kubeClient.BatchV1().Jobs(jobSpec.Namespace).Create(ctx, jobSpec, metav1.CreateOptions{})
+	generatedJob, err := jc.kubeClient.BatchV1().Jobs(jobSpec.Namespace).Create(ctx, jobSpec, metav1.CreateOptions{})
 	if err != nil {
 		return "", "", fmt.Errorf("failed to create Job: %w", err)
 	}
@@ -25,15 +34,15 @@ func (s *BackupServer) createJob(ctx context.Context, jobSpec *batchv1.Job) (str
 	return generatedJob.Name, generatedJob.Namespace, nil
 }
 
-func (s *BackupServer) createCronJob(ctx context.Context, cronJobSpec *batchv1.CronJob) (string, string, error) {
-	exCj, err := s.kubeClient.BatchV1().CronJobs(cronJobSpec.Namespace).Get(ctx, cronJobSpec.Name, metav1.GetOptions{})
+func (jc JobsCreator) CreateCronJob(ctx context.Context, cronJobSpec *batchv1.CronJob) (string, string, error) {
+	exCj, err := jc.kubeClient.BatchV1().CronJobs(cronJobSpec.Namespace).Get(ctx, cronJobSpec.Name, metav1.GetOptions{})
 	if apierrors.IsAlreadyExists(err) {
 		return exCj.Name, exCj.Namespace, ErrAlreadyExists
 	}
 	if err != nil && !apierrors.IsNotFound(err) {
 		return "", "", fmt.Errorf("Failed to check cj %s existence: %+v", cronJobSpec.Name, err)
 	}
-	generatedJob, err := s.kubeClient.BatchV1().CronJobs(cronJobSpec.Namespace).Create(ctx, cronJobSpec, metav1.CreateOptions{})
+	generatedJob, err := jc.kubeClient.BatchV1().CronJobs(cronJobSpec.Namespace).Create(ctx, cronJobSpec, metav1.CreateOptions{})
 	if apierrors.IsAlreadyExists(err) {
 		return exCj.Name, exCj.Namespace, ErrAlreadyExists
 	} else if err != nil {
