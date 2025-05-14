3ea661fd229cfd8ceda7bea61a3fc8e7ae609e6f
Anton Shadrin <a.shadrin@yadro.com>
2025-05-14 04:23:08 +0300

    Add EnvGetterMerger

--- servers/backup/stubs.go
+++ servers/backup/stubs.go
@@ -13,6 +13,25 @@ type EnvGetter interface {
 	GetEnvs() []corev1.EnvVar
 }
 
+type EnvGetterMerger struct {
+	envegetters []EnvGetter
+}
+
+func NewEnvGetterMerger(envgetters []EnvGetter) EnvGetterMerger {
+	return EnvGetterMerger{
+		envegetters: envgetters,
+	}
+}
+
+func (egm EnvGetterMerger) GetEnvs() []corev1.EnvVar {
+	envs := []corev1.EnvVar{}
+	for _, getter := range egm.envegetters {
+		envs = append(envs, getter.GetEnvs()...)
+	}
+
+	return envs
+}
+
 type JobsStub struct {
 	namePsfx      string
 	namespace     string
