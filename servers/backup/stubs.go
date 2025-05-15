22e96879bca41a7e46dda90704db2bc6e19aafe6
Anton Shadrin <a.shadrin@yadro.com>
2025-05-15 19:11:29 +0300

    Move envgetters

--- servers/backup/stubs.go
+++ servers/backup/stubs.go
@@ -9,29 +9,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-type EnvGetter interface {
-	GetEnvs() []corev1.EnvVar
-}
-
-type EnvGetterMerger struct {
-	envegetters []EnvGetter
-}
-
-func NewEnvGetterMerger(envgetters []EnvGetter) EnvGetterMerger {
-	return EnvGetterMerger{
-		envegetters: envgetters,
-	}
-}
-
-func (egm EnvGetterMerger) GetEnvs() []corev1.EnvVar {
-	envs := []corev1.EnvVar{}
-	for _, getter := range egm.envegetters {
-		envs = append(envs, getter.GetEnvs()...)
-	}
-
-	return envs
-}
-
 type JobsStub struct {
 	namePsfx      string
 	namespace     string
