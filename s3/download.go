e1e918599268de0b1961a3c7b4c37b2f8e6ea7d1
Svetlana Bolotina <svebo3348@gmail.com>
2025-05-23 23:32:19 +0300

    Update target revision by name (#9)

--- s3/download.go
+++ s3/download.go
@@ -33,16 +33,10 @@ func (d S3Downloader) Download(ctx context.Context, bucketName, databaseName, ba
 	backupRevision, err := strconv.Atoi(backupRevisionStr)
 	// Если backupRevisionStr - число, выбираем бэкап по индексу
 	if err == nil && backupRevision >= 0 {
-		backupKeys, err := d.listBackupFiles(ctx, databaseName, bucketName)
+		selectedBackupKey, err = d.GetBackupByRevision(ctx, backupRevision, databaseName, bucketName)
 		if err != nil {
 			return fmt.Errorf("failed to list backup files from S3: %v", err)
 		}
-		sort.Strings(backupKeys) // Сортируем по времени (по убыванию)
-
-		if backupRevision >= len(backupKeys) {
-			return fmt.Errorf("BACKUP_REVISION (%d) is out of range. Available backups: %d", backupRevision, len(backupKeys))
-		}
-		selectedBackupKey = backupKeys[backupRevision]
 	} else {
 		// Если backupRevisionStr - строка, ищем бэкап с таким именем
 		selectedBackupKey = backupRevisionStr
@@ -72,24 +66,25 @@ func (d S3Downloader) Download(ctx context.Context, bucketName, databaseName, ba
 }
 
 // listBackupFiles получает список файлов бэкапов из S3
-func (d S3Downloader) listBackupFiles(ctx context.Context, backupDir, bucketName string) ([]string, error) {
-	var backupKeys []string
-
-	paginator := s3.NewListObjectsV2Paginator(d.client, &s3.ListObjectsV2Input{
+func (d S3Downloader) GetBackupByRevision(ctx context.Context, backupRevision int, backupDir, bucketName string) (string, error) {
+	listOutput, err := d.client.ListObjectsV2(ctx, &s3.ListObjectsV2Input{
 		Bucket: aws.String(bucketName),
 		Prefix: aws.String(ensureTrailingSlash(backupDir)),
 	})
-
-	for paginator.HasMorePages() {
-		page, err := paginator.NextPage(ctx)
-		if err != nil {
-			return nil, err
-		}
-
-		for _, obj := range page.Contents {
-			backupKeys = append(backupKeys, *obj.Key)
-		}
+	if err != nil {
+		return "", fmt.Errorf("failed to list objects: %+v", err)
 	}
 
-	return backupKeys, nil
+	objects := listOutput.Contents
+	var selectedBackupKey string
+	if backupRevision < len(objects) {
+		sort.Slice(objects, func(i, j int) bool {
+			return objects[j].LastModified.Before(*objects[i].LastModified)
+		})
+		selectedBackupKey = *objects[backupRevision].Key
+	} else {
+		return "", fmt.Errorf("BACKUP_REVISION (%d) is out of range. Available backups: %d", backupRevision, len(objects))
+	}
+
+	return selectedBackupKey, nil
 }
