e6f66b30f90d8cd5e6566f49cfc71755bbf823a9
s.bolotina <s.bolotina@yadro.com>
2025-05-25 13:49:18 +0300

    Add multipart download

--- s3/download.go
+++ s3/download.go
@@ -3,7 +3,7 @@ package s3
 import (
 	"context"
 	"fmt"
-	"os"
+	"io"
 	"sort"
 	"strconv"
 
@@ -27,7 +27,7 @@ func NewS3Downloader(ctx context.Context, endpoint, accessKey, secretKey, region
 }
 
 // downloadBackupFromS3 скачивает выбранный бэкап из S3 в локальный файл
-func (d S3Downloader) Download(ctx context.Context, bucketName, databaseName, backupRevisionStr, localFilePath string) error {
+func (d S3Downloader) Download(ctx context.Context, bucketName, databaseName, backupRevisionStr string, fileContent io.Writer) error {
 	var selectedBackupKey string
 
 	backupRevision, err := strconv.Atoi(backupRevisionStr)
@@ -51,15 +51,18 @@ func (d S3Downloader) Download(ctx context.Context, bucketName, databaseName, ba
 	}
 	defer resp.Body.Close()
 
-	file, err := os.Create(localFilePath)
-	if err != nil {
-		return fmt.Errorf("failed to create local file: %v", err)
-	}
-	defer file.Close()
+	buffer := make([]byte, partSize)
 
-	_, err = file.ReadFrom(resp.Body)
-	if err != nil {
-		return fmt.Errorf("failed to write S3 object to local file: %v", err)
+	for {
+		bytesRead, err := resp.Body.Read(buffer)
+		if bytesRead == 0 || err == io.EOF {
+			break
+		}
+
+		_, err = fileContent.Write(buffer)
+		if err != nil {
+			return fmt.Errorf("failed to write S3 object to file: %v", err)
+		}
 	}
 
 	return nil
